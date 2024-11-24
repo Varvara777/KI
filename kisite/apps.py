8ebc4a20c5e85c0af17168e384985bdd6f09272c
Varvara777 <wwwpugovka@yandex.ru>
2024-11-25 00:11:46 +0500

    Минимальная настройка, проба моделей

---
+++ kisite/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KisiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kisite'
---
+++ kisite/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Tovar(models.Model):
+    title=models.CharField(max_length=150,verbose_name='Наименование')
+    description=models.TextField(verbose_name='Описание')
+    image=models.ImageField(upload_to='images',verbose_name='Изображение')
+    price=models.DecimalField(max_digits=10,decimal_places=2,verbose_name='Цена')
+
+    TYPE=[
+        ('DO3','Дети до трех лет'),
+        ('GRL3_6','Девочки 3-6'),
+        ('BOY3_6','Мальчики 3-6'),
+        ('GRL7_13','Девочки 7-13'),
+        ('GRL7_13','Девочки 7-13'),
+        ('ForParents','Настолки для мам и пап')
+
+    ]
+    type=models.CharField(choices=TYPE,max_length=15,default='DO3',verbose_name='Тип')
+
